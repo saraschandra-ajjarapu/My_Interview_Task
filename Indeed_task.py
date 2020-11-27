27c43d1f003fe79509ef615abae70dde937b2dfd
saraschandra-ajjarapu <56125388+saraschandra-ajjarapu@users.noreply.github.com>
2020-11-26 19:34:54 -0600

    Create Indeed_task.py
    Indeed Interview task through karat

---
+++ Indeed_task.py
@@ -0,0 +1,169 @@
+'''
+Imagine we have an image. We'll represent this image as a simple 2D array where every pixel is a 1 or a 0. The image you get is known to have a single rectangle of 0s on a background of 1s.
+
+Write a function that takes in the image and returns one of the following representations of the rectangle of 0's: top-left coordinate and bottom-right coordinate OR top-left coordinate, width, and height.
+
+image1 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+]
+
+Sample output variations (only one is necessary):
+
+findRectangle(image1) =>
+  x: 3, y: 2, width: 3, height: 2
+  2,3 3,5 -- row,column of the top-left and bottom-right corners
+
+Other test cases:
+
+image2 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 0],
+]
+
+findRectangle(image2) =>
+  x: 6, y: 4, width: 1, height: 1
+  4,6 4,6
+
+image3 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0],
+]
+
+findRectangle(image3) =>
+  x: 5, y: 3, width: 2, height: 2
+  3,5 4,6
+  
+image4 = [
+  [0, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+]
+
+findRectangle(image4) =>
+  x: 0, y: 0, width: 1, height: 1
+  0,0 0,0
+
+image5 = [
+  [0],
+]
+
+findRectangle(image5) =>
+  x: 0, y: 0, width: 1, height: 1
+  0,0 0,0
+
+n: number of rows in the input image
+m: number of columns in the input image
+'''
+
+image1 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+]
+
+image2 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 0],
+]
+
+image3 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0],
+]
+
+image4 = [
+  [0, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+]
+
+image5 = [
+  [0],
+]
+
+#rectangles filled with a
+def findend (i,j,a,output, index):
+    x = len(a)
+    y = len (a[0])
+    
+#column edge case
+    flagc = 0
+
+#row edge case
+    flagr = 0
+
+#loop breaks first 1 encounters
+    for m in range(i,x):
+        if a[m][j] == 1:
+            flagr = 1 
+            break
+        #if a[m][j] == 5:
+           # pass
+        
+    for n in range (j,y):
+        if a[m][n] == 1:
+            flagc = 1
+            break
+       # a[m][n] = 5
+
+    if flagr == 1:
+        output [index].append(m-1)
+        print (m-1)
+    else:
+        output[index].append(m)
+    if flagc == 1:
+         output [index].append(n-1)
+    else:
+        output[index].append(n)
+        
+    
+def get_rectangle_coordinates(a):
+    size_of_array = len(a)
+    output = []
+    index = -1 
+    
+    flag = False
+    for i in range(0,size_of_array):
+        for j in range(0,len(a[0])):
+            print(flag)
+            if a[i][j] == 0 and flag == False: 
+                flag = True
+                output.append([i, j])
+                index = index + 1
+                findend(i,j,a,output, index)
+
+    print (output)
+            
+
+tests = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 0],
+]
+
+
+get_rectangle_coordinates(tests)
